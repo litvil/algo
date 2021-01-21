365d61ddbc7b2bdd398f7e5c8d1295ecf79ea7dc
Illia Litviak <illia.litviak@ins-connect.com>
2021-01-21 09:10:48 -0500

    BinaryGap task

--- src/test/java/com/litvil/algo/solutions/BinaryGapTest.java
+++ src/test/java/com/litvil/algo/solutions/BinaryGapTest.java
@@ -7,13 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class IntegerToBinaryTest {
+class BinaryGapTest {
   @Test
   @DisplayName("Run")
   public void test1() {
-    assertEquals("10001", IntegerToBinary.convert(17));
-    assertEquals("1000010001", IntegerToBinary.convert(529));
-    assertEquals("10000010001", IntegerToBinary.convert(1041));
-    assertEquals("100000", IntegerToBinary.convert(32));
+
+    assertEquals(5, BinaryGap.solution(1041));
+    assertEquals(4, BinaryGap.solution(529));
+    assertEquals(1, BinaryGap.solution(20));
+    assertEquals(0, BinaryGap.solution(15));
+
   }
 }
